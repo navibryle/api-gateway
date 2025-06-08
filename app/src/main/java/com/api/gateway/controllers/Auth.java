912d8cb8d9416976874fb107a12b4d1a314f423e
navibryle <naviivan321@gmail.com>
2025-06-08 15:40:21 -0700

    add login

--- app/src/main/java/com/api/gateway/controllers/Auth.java
+++ app/src/main/java/com/api/gateway/controllers/Auth.java
@@ -17,7 +17,6 @@ import com.api.gateway.Util;
 import com.api.gateway.models.ApiModel;
 import com.api.gateway.models.AuthRequest;
 import com.api.gateway.models.AuthResponse;
-import com.api.gateway.models.RegisterRequest;
 import com.api.gateway.reader.ConfigDefinition;
 import com.api.gateway.security.JwsUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -52,10 +51,6 @@ public class Auth {
     return getApiTarget(GatewayController.PREFIX + "login");
   }
 
-  private String getRegistrationUrlForApi() throws AuthException{
-    return getApiTarget(GatewayController.PREFIX + "registration");
-  }
-
   @PostMapping("/login")
   public AuthResponse login(@RequestBody AuthRequest loginForm, HttpServletResponse response) throws RestClientException, AuthException, JsonProcessingException{
     HttpHeaders headers = new HttpHeaders();
@@ -67,27 +62,6 @@ public class Auth {
       out.setBearerToken(jwsUtil.getJws(loginForm.getUsername()));
       out.setStatus(AuthResponse.Status.OK);
     }else{
-      out.setStatus(AuthResponse.Status.NOT_OK);
-    }
-    return out;
-  }
-
-  @PostMapping("/register")
-  public AuthResponse register(@RequestBody RegisterRequest registrationForm,HttpServletResponse response) throws RestClientException, AuthException, JsonProcessingException{
-    HttpHeaders headers = new HttpHeaders();
-    HttpEntity<String> httpEntity = new HttpEntity<String>(util.getObjectAsJson(registrationForm),headers);
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    ResponseEntity<String> authResponse = authRestTemplate.postForEntity(getRegistrationUrlForApi(), httpEntity,String.class);
-    AuthResponse out = new AuthResponse();
-    if (authResponse.getStatusCode().is2xxSuccessful()){
-      out.setBearerToken(jwsUtil.getJws(registrationForm.getUsername()));
-      out.setStatus(AuthResponse.Status.OK);
-    }else{
-      if (authResponse.hasBody()){
-        if (authResponse.getBody().toLowerCase().contains("user_exists")){
-          throw new AuthException("User already exists");
-        }
-      }
       out.setStatus(AuthResponse.Status.NOT_OK);
     }
     return out;
